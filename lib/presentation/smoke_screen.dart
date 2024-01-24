9f1fc7278ecb0ca2fbbf4716d44dd7b723c656d3
raphael <raphael.birkl@gmail.com>
2024-01-24 20:24:02 +0100

    drawer improved

--- lib/presentation/smoke_screen.dart
+++ lib/presentation/smoke_screen.dart
@@ -18,25 +18,96 @@ class _SmokeSignalScreenState extends State<SmokeSignalScreen> {
           padding: EdgeInsets.zero,
           children: [
             const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
+              child: Text(
+                'specify your signal',
+                style: TextStyle(fontSize: 25),
               ),
-              child: Text('Drawer Header'),
             ),
             ListTile(
-              title: const Text('Item 1'),
+              leading: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.radio_button_unchecked)),
+              title: const Text('pending violence'),
               onTap: () {
                 // Update the state of the app.
                 // ...
               },
             ),
             ListTile(
-              title: const Text('Item 2'),
+              leading: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.radio_button_unchecked)),
+              title: const Text('first aid meassures'),
               onTap: () {
                 // Update the state of the app.
                 // ...
               },
             ),
+            ListTile(
+              leading: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.radio_button_unchecked)),
+              title: const Text('evacuation'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              leading: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.radio_button_unchecked)),
+              title: const Text('tracing after crime'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            const Spacer(),
+            const Spacer(),
+            ListTile(
+              leading: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.check_box_outline_blank)),
+              title: const Text("> 10 delinquents"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.check_box_outline_blank)),
+              title: const Text("drug abuse"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.check_box_outline_blank)),
+              title: const Text("weapons involved"),
+              onTap: () {},
+            ),
+            const Divider(),
+            Row(
+              children: [
+                const Spacer(),
+                ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Set Signal",
+                      style: TextStyle(color: Colors.white),
+                    )),
+                const SizedBox(width: 11),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                  ),
+                  child: const Text("Cancel",
+                      style: TextStyle(color: Colors.black)),
+                ),
+              ],
+            )
           ],
         ),
       ),
