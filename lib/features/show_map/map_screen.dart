d1c6c6724869f37a686f968fcc1aa92ed9b0cd87
raphael <raphael.birkl@gmail.com>
2024-02-10 21:19:02 +0100

    map centering function added

--- lib/features/show_map/map_screen.dart
+++ lib/features/show_map/map_screen.dart
@@ -16,6 +16,7 @@ class _MapScreenState extends State<MapScreen> {
   bool _serviceEnabled = false;
   PermissionStatus _permissionGranted = PermissionStatus.denied;
   LocationData? _locationData;
+  MapController mapController = MapController();
 
   @override
   void initState() {
@@ -43,6 +44,15 @@ class _MapScreenState extends State<MapScreen> {
     setState(() {});
   }
 
+  void _moveToCurrentLocation() {
+    if (_locationData != null) {
+      mapController.move(
+        LatLng(_locationData!.latitude!, _locationData!.longitude!),
+        14.0, // Zoomlevel
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     if (_locationData == null) {
@@ -59,37 +69,72 @@ class _MapScreenState extends State<MapScreen> {
               ? Text("${_locationData!.latitude}, ${_locationData!.longitude}")
               : const Text("Loading..."),
         ),
-        body: FlutterMap(
-          // OPTIONS
-          options: MapOptions(
-            initialCenter: _locationData != null
-                ? LatLng(_locationData!.latitude ?? 48.137154,
-                    _locationData!.longitude ?? 11.576124)
-                : const LatLng(48.137154, 11.576124),
-            initialZoom: 14,
-          ),
-
-          // LAYERS
+        body: Stack(
           children: [
-            TileLayer(
-              urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-            ),
-            const RichAttributionWidget(
-              attributions: [
-                TextSourceAttribution('OpenStreetMap contributors'),
+            FlutterMap(
+              mapController: mapController,
+              // OPTIONS
+              options: MapOptions(
+                initialCenter: _locationData != null
+                    ? LatLng(_locationData!.latitude ?? 48.137154,
+                        _locationData!.longitude ?? 11.576124)
+                    : const LatLng(48.137154, 11.576124),
+                initialZoom: 14,
+              ),
+
+              // LAYERS
+              children: [
+                TileLayer(
+                  urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                ),
+                const RichAttributionWidget(
+                  attributions: [
+                    TextSourceAttribution('OpenStreetMap contributors'),
+                  ],
+                ),
+                MarkerLayer(markers: [
+                  Marker(
+                      point: _locationData != null
+                          ? LatLng(_locationData!.latitude ?? 30,
+                              _locationData!.longitude ?? 40)
+                          : const LatLng(30, 40),
+                      child: const Icon(
+                        Icons.local_activity_outlined,
+                        color: dutyRed,
+                      ))
+                ])
               ],
             ),
-            MarkerLayer(markers: [
-              Marker(
-                  point: _locationData != null
-                      ? LatLng(_locationData!.latitude ?? 30,
-                          _locationData!.longitude ?? 40)
-                      : const LatLng(30, 40),
-                  child: const Icon(
-                    Icons.local_activity_outlined,
-                    color: dutyRed,
-                  ))
-            ])
+            Positioned(
+              right: 0,
+              top: (MediaQuery.of(context).size.height - 70) /
+                  2, // Vertikal zentriert
+              child: Container(
+                  width: 60,
+                  height: 160,
+                  color: dutyWhite,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      GestureDetector(
+                        onTap: () {},
+                        child: const Icon(Icons.radio_button_unchecked,
+                            color: dutyRed),
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: const Icon(Icons.turn_right_outlined,
+                            color: dutyBlack),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          _moveToCurrentLocation();
+                        },
+                        child: const Icon(Icons.adjust, color: dutyBlack),
+                      )
+                    ],
+                  )),
+            ),
           ],
         ),
       );
